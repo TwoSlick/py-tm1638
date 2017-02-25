835163f72e23804052895825524ca5fc4ac1b616
Justin Foley <bitbucket@foley2000.com>
2017-02-25 22:40:09 +0000

    Script to show the current local ip on the display

---
+++ show_ip.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# TM1638 playground
+
+import TM1638
+import time
+import socket
+import sys
+
+
+# These are the pins the display is connected to. Adjust accordingly.
+# In addition to these you need to connect to 5V and ground.
+
+DIO = 17
+CLK = 21
+STB = 22
+
+display = TM1638.TM1638(DIO, CLK, STB)
+
+display.enable(1)
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 0))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+ips = get_ip()
+display.scroll_text(ips)
